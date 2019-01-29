4ea165c223ee9242c40e2242dafe902561f1805a
Jiawei <34206537+piggggg@users.noreply.github.com>
2019-01-29 22:25:34 +0800

    Add files via upload

---
+++ client/app.js
@@ -0,0 +1,115 @@
+
+'use strict';
+
+var app = angular.module('application', []);
+
+
+app.controller('appController', function($scope, appFactory){
+
+	$("#success_holder").hide();
+	$("#success_create").hide();
+	$("#error_holder").hide();
+	$("#error_query").hide();
+	$("#error_create").hide();
+
+	$scope.queryAllPrfixes = function(){
+
+		var prefixes = $scope.prefixes;
+
+		appFactory.queryAllPrfixes(prefixes, function(data){
+			var array = [];
+			for (var i = 0; i < data.length; i++){
+				parseInt(data[i].Key);
+				data[i].Record.Key = parseInt(data[i].Key);
+				array.push(data[i].Record);
+			}
+			array.sort(function(a, b) {
+			    return parseFloat(a.Key) - parseFloat(b.Key);
+			});
+			$scope.all_prefixes = array;
+		});
+	}
+
+	$scope.queryPrefix = function(){
+
+		var prefix = $scope.ipPrefix;
+		console.log(prefix)
+
+		appFactory.queryPrefix(prefix, function(data){
+			$scope.query_prefix = data;
+
+			if ($scope.query_prefix == "Could not query IPv6 prefix"){
+				console.log()
+				$("#error_query").show();
+			} else{
+				$("#error_query").hide();
+			}
+		});
+	}
+
+	$scope.allocatePrefix = function(){
+
+		appFactory.allocatePrefix($scope.prefix, function(data){
+			$scope.allocate_prefix = data;
+			if ($scope.allocate_prefix == "Could not allocate this IPv6 prefix" || $scope.allocate_prefix.indexOf("Failed to allocate IPv6 prefix") >= 0){
+				console.log()
+				$("#error_create").show();
+				$("#success_create").hide();
+			} else{
+				$("#error_create").hide();
+				$("#success_create").show();
+			}
+		});
+	}
+
+	$scope.changeHolder = function(){
+		console.log($scope.holder)
+		appFactory.changeHolder($scope.holder, function(data){
+			$scope.change_holder = data;
+		});
+	}
+
+});
+
+app.factory('appFactory', function($http){
+
+	var factory = {};
+
+    factory.queryAllPrfixes = function(data, callback){
+
+    	var prefixes = data.startKey.replace('/', '@') + '-' + data.endKey.replace('/', '@');
+
+    	$http.get('/get_all_prefixes/'+prefixes).success(function(output){
+			callback(output)
+		});
+	}
+
+	factory.queryPrefix = function(prefix, callback){
+
+		var address = prefix.replace('/', '@')
+
+    	$http.get('/get_prefix/'+address).success(function(output){
+			callback(output)
+		});
+	}
+
+	factory.allocatePrefix = function(data, callback){
+
+		var ipPrefx = data.IPv6_prefix.replace('/', '@');
+		var prefix = ipPrefx + "-" + data.AS_number + "-" + data.Assign_by + "-" + data.Assign_to + "-" + data.Advertisement + "-" + new Date().getTime();
+    	$http.get('/allocate_prefix/'+prefix).success(function(output){
+			callback(output)
+		});
+	}
+
+	factory.changeHolder = function(data, callback){
+
+		var holder = data.prefix.replace('/', '@') + "-" + data.asNumber + "-" + data.assignTo + "-" + new Date().getTime();
+
+    	$http.get('/change_holder/'+holder).success(function(output){
+			callback(output)
+		});
+	}
+
+	return factory;
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+
+var express       = require('express');        // call express
+var app           = express();                 // define our app using express
+var bodyParser    = require('body-parser');
+var http          = require('http')
+var fs            = require('fs');
+var Fabric_Client = require('fabric-client');
+var path          = require('path');
+var util          = require('util');
+var os            = require('os');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var app = express();
+
+
+require('./routes.js')(app);
+
+
+app.use(express.static(path.join(__dirname, './client')));
+
+
+var port = process.env.PORT || 8000;
+
+
+app.listen(port,function(){
+  console.log("Live on port: " + port);
+});
